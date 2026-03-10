33abeedf02dec79cc596520fa9f3b79b09d6dee0
Joey-Cerulli <cerullij@union.edu>
2026-03-10 15:26:04 -0400

    added a omment

--- main/bt_app_av.c
+++ main/bt_app_av.c
@@ -655,6 +655,7 @@ void bt_app_a2d_data_cb(const uint8_t *data, uint32_t len)
     int16_t *samples = (int16_t *)data;
     int sample_count = len / 2;
 
+    //Scale left and right volumes according to variables
     for (int i = 0; i < sample_count; i+=2) {
         samples[i] = samples[i] * SET_LVOL;
         samples[i+1] = samples[i+1] * SET_RVOL;
